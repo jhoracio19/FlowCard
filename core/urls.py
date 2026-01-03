f3e18038373b7594b93a799c7cff308c36bdd1f9
Jose Horacio <jhoracio19@hotmail.com>
2026-01-02 23:53:12 -0600

    ui: redirección automática de la raíz al login

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from cards.views import RegisterView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cards.urls')),
+    path('', RedirectView.as_view(url='/login/', permanent=True)),
     path('statements/', include('statements.urls')),
     
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
